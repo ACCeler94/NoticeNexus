b32faf9f30398b9b29b5e2b2dfcafa6976f42be7
Artur C <araghast94@icloud.com>
2023-11-17 15:34:31 +0100

    Modify adForm behavior

--- client/src/components/features/AdForm/AdForm.js
+++ client/src/components/features/AdForm/AdForm.js
@@ -6,41 +6,61 @@ import CircularProgress from '@mui/material/CircularProgress';
 import { useDispatch, useSelector } from 'react-redux';
 import { DateTime } from "luxon";
 import { useNavigate, useParams } from 'react-router-dom';
-import { addNewAd, fetchById, updateAd } from '../Ads/adsSlice';
+import { addNewAd, fetchById, resetNewAdStatus, updateAd } from '../Ads/adsSlice';
+import { Alert } from '@mui/material';
 
 
+// [TODO] fix editing ad adding posts only for logged users
 const AdForm = () => {
   const user = useSelector(state => state.users.user);
+  const users = useSelector(state => state.users);
+  //console.log(users)
   const dispatch = useDispatch();
   const currentDate = DateTime.now().toLocaleString();
   const params = useParams();
-  const status = useSelector(state => state.ads.status);
+  const status = useSelector(state => state.ads.adFormStatus);
   const error = useSelector(state => state.ads.error);
   const navigate = useNavigate();
   const currentAd = useSelector(state => state.ads.currentAd);
 
-  let id;
-  if (params) {
-    id = params.id
-  }
+  const { id } = params;
+  const [lastId, setLastId] = useState(currentAd && currentAd.id) || null; // if the user goes to the edit through the button on singleAdPage then there is no need to fetch currentAd from the server
 
   // if id exists and is different than currentAd then fetch ad by id
   useEffect(() => {
-    if (id) {
-      if (!currentAd.id || currentAd.id !== id) {
-        dispatch(fetchById(id));
+    if (id && id !== lastId) { // if the user uses url instead of edit button then ad with id from url needs to be fetched
+      dispatch(fetchById(id));
+
+    }
+  }, [dispatch, id, lastId])
+
+  // redirect after 1 sec on success
+  useEffect(() => {
+    if (status === 'success') {
+      const timer = setTimeout(() => {
+        navigate('/');
+      }, 1000);
+
+      return () => {
+        clearTimeout(timer); //clear the timeout if the component unmounts before the redirection happens
+        dispatch(resetNewAdStatus())
       }
     }
-  }, [dispatch, id, currentAd])
+  }, [status, navigate, dispatch]);
 
 
+  let userId
+  if (user) {
+    userId = user.id
+  }
 
-  const [title, setTitle] = useState(currentAd.title || '');
-  const [desc, setDesc] = useState(currentAd.desc || '');
-  const [date] = useState(currentAd.date || currentDate);
-  const [photo, setPhoto] = useState(currentAd.photo || null);
-  const [price, setPrice] = useState(currentAd.price || '');
-  const [seller] = useState(user.id);
+  const [title, setTitle] = useState(currentAd && currentAd.title ? currentAd.title : '');
+  const [desc, setDesc] = useState(currentAd && currentAd.desc ? currentAd.desc : '');
+  const [date] = useState(currentAd && currentAd.date ? currentAd.date : currentDate);
+  const [photo, setPhoto] = useState(currentAd && currentAd.photo ? currentAd.photo : null);
+  const [price, setPrice] = useState(currentAd && currentAd.price ? currentAd.price : '');
+  const [location, setLocation] = useState(currentAd && currentAd.location ? currentAd.location : '');
+  const [seller, setSeller] = useState(userId || '');
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -51,6 +71,7 @@ const AdForm = () => {
     newAdData.append('date', date);
     newAdData.append('photo', photo);
     newAdData.append('price', price);
+    newAdData.append('location', location);
     newAdData.append('seller', seller)
 
     // check if id exists, if it does then dispatch updateAd if not then adNewAd
@@ -59,6 +80,13 @@ const AdForm = () => {
     } else {
       dispatch(addNewAd(newAdData))
     }
+    // reset form inputs
+    // Reset form fields
+    setTitle('');
+    setDesc('');
+    setPhoto(null);
+    setPrice('');
+    setLocation('');
   }
 
 
@@ -82,18 +110,36 @@ const AdForm = () => {
     )
   };
 
+  if (status === 'success') {
+    return (
+      <Alert severity='success'>Ad sent successfully.</Alert>
+    )
+  }
+
   return (
     <div className={styles.formWrapper}>
       <h2>Sign Up</h2>
       <form className={styles.form} onSubmit={handleSubmit}>
 
 
-        <TextField id="title" label="Title" variant="outlined" sx={{ margin: '5px 0' }} value={title} onChange={e => setTitle(e.target.value)} required />
+        <TextField id="title" label="Title" variant="outlined" sx={{ margin: '5px 0' }} value={title} onChange={e => setTitle(e.target.value)} required InputProps={{
+          inputProps: {
+            min: 10,
+            max: 50,
+          },
+        }} />
 
         <TextField id="desc" label="Description" variant="outlined" sx={{ margin: '5px 0' }} value={desc} onChange={e => setDesc(e.target.value)} multiline
-          rows={4} required />
+          rows={4} required InputProps={{
+            inputProps: {
+              min: 20,
+              max: 1000,
+            },
+          }} />
 
-        <TextField id="price" label="Description" variant="outlined" sx={{ margin: '5px 0' }} value={price} onChange={e => setPrice(e.target.value)} type='number' required />
+        <TextField id="price" label="Price" variant="outlined" sx={{ margin: '5px 0' }} value={price} onChange={e => setPrice(e.target.value)} type='number' required />
+
+        <TextField id="location" label="Location" variant="outlined" sx={{ margin: '5px 0' }} value={location} onChange={e => setLocation(e.target.value)} required />
 
         <TextField id="photo" variant="outlined" type="file" helperText="Select Your Avatar" sx={{ margin: '5px 0' }} onChange={e => setPhoto(e.target.files[0])} required />
 
